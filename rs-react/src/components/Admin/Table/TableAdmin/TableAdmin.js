f29ae3da2a01124ddb0ef229ebabd711dc078a37
RominKings <gudinne@outlook.com>
2022-10-08 04:28:10 -0300

    2.4.2
    Clase 101

--- rs-react/src/components/Admin/Table/TableAdmin/TableAdmin.js
+++ rs-react/src/components/Admin/Table/TableAdmin/TableAdmin.js
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { size } from 'lodash';
 import { ReactComponent as IcTable } from "../../../../assets/mesa.svg";
-import { Button } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
+import classNames from "classnames";
+import { getOrdersByTableApi } from '../../../../api/orders';
+import { ORDER_STATUS } from '../../../../utils/constants';
 import "./TableAdmin.css";
 
 export function TableAdmin(props) {
   const {table} = props;
+  const [orders, setOrders] = useState([]);
+  console.log(orders);
+
+  useEffect(() => {
+    (async() => {
+      const response = await getOrdersByTableApi(table.id, ORDER_STATUS.PENDING)
+      setOrders(response);
+    })()
+  }, [])
 
   return (
     <div className='table-admin'>
-      <h1><IcTable id="mesa" /></h1>
+      {size(orders) > 0 ? (
+        <Card bsPrefix='card' bg='warning'><Card.Body>{size(orders)}</Card.Body></Card>
+      ) : null }
+      <h1><IcTable id="mesa" className={classNames({
+        pending: size(orders) > 0,
+      })} /></h1>
       <p>Mesa {table.number}</p>
     </div>
   )
--- rs-react/src/components/Admin/Table/TablesListAdmin/TableListAdmin.js
+++ rs-react/src/components/Admin/Table/TablesListAdmin/TableListAdmin.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { FiRefreshCcw } from 'react-icons/fi';
 import {map, size} from 'lodash';
 import { TableAdmin } from '../TableAdmin';
+import "./TableListAdmin.css";
 
 
 export function TableListAdmin(props) {
@@ -8,6 +11,13 @@ export function TableListAdmin(props) {
 
   return (
     <div className='table-admin'>
+      <Button variant="info" id='reload' onClick={() => console.log('OnRefetchReload')}><FiRefreshCcw/></Button>
+        
+        <div className='reload-toggle' >
+          <span id='span-reload'>Reload automatico</span>
+          <Form.Check aria-label="option 1" id='check-reload' toggle="true" onChange={(_, ...data) => console.log(data.checked)} />
+        </div>
+        
         {map(tables, (table) => (
             <TableAdmin key={table.number} table={table}/>
         ))}
