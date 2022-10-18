d0551920c39fa201715c9044de05d88c19e9c24b
RominKings <gudinne@outlook.com>
2022-10-17 23:27:30 -0300

    Revert "2.6"
    This reverts commit a2334e1f391d09539f0a72587c866cc784ef55e6.

--- rs-react/src/pages/Client/index.js
+++ rs-react/src/pages/Client/index.js
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 export * from "./SelectTable";
-=======
+
 export * from "./SelectTable";
+
 export * from "./Categories";
->>>>>>> parent of a2334e1 (2.6)
--- rs-react/src/routes/routes.client.js
+++ rs-react/src/routes/routes.client.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-import {ClientLayout, BasicLayout} from "../Layouts";
-import { SelectTable } from "../pages/Client";
-=======
 import {BasicLayout,ClientLayout} from "../Layouts";
 import {SelectTable ,Categories} from "../pages/Client";
->>>>>>> parent of a2334e1 (2.6)
 
 
 const routesClient = [
@@ -13,17 +8,14 @@ const routesClient = [
         layout: BasicLayout,
         component: SelectTable,
         exct: true,
-<<<<<<< HEAD
-    }
-=======
     },
+    
     {
         path: "/client/:tableNumber",
         layout: ClientLayout,
         component: Categories,
         exact: true,
     },
->>>>>>> parent of a2334e1 (2.6)
 ]
 
 export default routesClient;
